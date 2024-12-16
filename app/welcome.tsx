8bb21b95a2c3b1e0eafc8869c1a55f0250109773
saish-spaciee <saish@spaciee.com>
2024-12-16 14:41:45 +0530

    fix: added getting started screen and related components

--- app/welcome.tsx
+++ app/welcome.tsx
@@ -1,14 +1,107 @@
-import ScreenWrapper from "@/components/screenWrapper";
-import { Text, StyleSheet } from 'react-native'
+import ScreenWrapper from "../components/screenWrapper";
+import {
+  Text,
+  StyleSheet,
+  View,
+  Image,
+  StyleProp,
+  ViewStyle,
+  TextStyle,
+  ImageStyle,
+  Pressable,
+} from "react-native";
+import { StatusBar } from "expo-status-bar";
+import { hp, wp } from "../helpers/common";
+import { theme } from "../constants/theme";
+import Button from "@/components/Button";
 
 const Welcome = () => {
-    return (
-        <ScreenWrapper>
-            <Text>Welcome!</Text>
-        </ScreenWrapper>
-    )
-}
+  return (
+    <ScreenWrapper bg="white">
+      <StatusBar style="dark" />
+      <View style={styles.container as StyleProp<ViewStyle>}>
+        <Image
+          style={styles.welcomeImage as StyleProp<ImageStyle>}
+          resizeMode="contain"
+          source={require("../assets/images/welcome.png")}
+        />
+        {/* title */}
 
-export default  Welcome;
+        <View style={{ gap: 20 }}>
+          <Text style={styles.title as StyleProp<TextStyle>}>LinkUp!</Text>
+          <Text style={styles.punchline as StyleProp<TextStyle>}>
+            Where every thought finds a home and every image tells a story!
+          </Text>
+        </View>
 
-const styles = StyleSheet.create({})
+        <View style={styles.footer as StyleProp<ViewStyle>}>
+          <Button
+            title="Getting Started"
+            buttonStyle={{ marginHorizontal: wp(3) }}
+          />
+          <View style={styles.bottomTextContainer as StyleProp<ViewStyle>}>
+            <Text style={styles.loginText as StyleProp<TextStyle>}>
+              Already have an account?
+            </Text>
+            <Pressable>
+              <Text
+                style={[
+                  styles.loginText as StyleProp<TextStyle>,
+                  {
+                    color: theme.colors.primaryDark,
+                    fontWeight: theme.fonts.bold,
+                  } as StyleProp<TextStyle>,
+                ]}
+              >
+                login
+              </Text>
+            </Pressable>
+          </View>
+        </View>
+      </View>
+    </ScreenWrapper>
+  );
+};
+
+export default Welcome;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "space-around",
+    backgroundColor: "white",
+    paddingHorizontal: wp(4),
+  },
+  welcomeImage: {
+    height: hp(30),
+    width: wp(80),
+  },
+  title: {
+    color: theme.colors.text,
+    fontSize: hp(4),
+    textAlign: "center",
+    fontWeight: `${theme.fonts.bold}`,
+  },
+  punchline: {
+    textAlign: "center",
+    paddingHorizontal: wp(4),
+    fontSize: hp(1.7),
+    color: theme.colors.text,
+  },
+  footer: {
+    width: "100%",
+    gap: 30,
+  },
+  bottomTextContainer: {
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+    gap: 5,
+  },
+  loginText: {
+    textAlign: "center",
+    color: theme.colors.text,
+    fontSize: hp(1.6),
+  },
+});
--- components/screenWrapper.tsx
+++ components/screenWrapper.tsx
@@ -4,7 +4,6 @@ import {useSafeAreaInsets} from "react-native-safe-area-context";
 const ScreenWrapper = ({children, bg}: any) => {
     const { top } = useSafeAreaInsets();
     const paddingTop = top > 0 ? top + 5 : 30;
-    console.log('padding', paddingTop)
     return (
         <View style={{flex: 1, paddingTop, backgroundColor: bg}}>
             {
